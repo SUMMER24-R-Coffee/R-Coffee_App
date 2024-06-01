0d69163cdd70ddddc42e342c78641f088c175d36
Đạt <datlowashere@gmail.com>
2024-06-01 15:15:36 +0700

    set up icons

---
+++ app/src/main/java/datlowashere/project/rcoffee/view/activity/SplashSceen.kt
@@ -0,0 +1,12 @@
+package datlowashere.project.rcoffee.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datlowashere.project.rcoffee.R
+
+class SplashSceen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_sceen)
+    }
+}
