c2c278ef3c2e78ec061ec98206be3e0ed23cc60e
Đạt <datlowashere@gmail.com>
2024-06-02 23:30:23 +0700

    set up navigation

--- app/src/main/java/datlowashere/project/rcoffee/ui/view/activity/SplashSceen.kt
+++ app/src/main/java/datlowashere/project/rcoffee/ui/view/activity/SplashSceen.kt
@@ -1,12 +1,12 @@
-package datlowashere.project.rcoffee.view.activity
+package datlowashere.project.rcoffee.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import datlowashere.project.rcoffee.R
 
-class Login : AppCompatActivity() {
+class SplashSceen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_splash_sceen)
     }
 }
