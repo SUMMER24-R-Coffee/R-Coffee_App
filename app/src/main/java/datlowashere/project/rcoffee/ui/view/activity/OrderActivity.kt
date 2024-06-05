9a753ed4e759446572b1681dd7b38f398e3895eb
Đạt <datlowashere@gmail.com>
2024-06-05 22:40:25 +0700

    update layout items

---
+++ app/src/main/java/datlowashere/project/rcoffee/ui/view/activity/OrderActivity.kt
@@ -0,0 +1,12 @@
+package datlowashere.project.rcoffee.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datlowashere.project.rcoffee.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
