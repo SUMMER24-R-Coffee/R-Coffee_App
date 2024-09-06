ee10e901471b6fb1e55f2d9fe13cb973e32a96d9
datlowashere <datlowashere@gmail.com>
2024-09-06 11:43:00 +0700

    create layout request reset password

---
+++ app/src/main/java/datlowashere/project/rcoffee/ui/view/activity/account/ResetPasswordRequestActivity.kt
@@ -0,0 +1,21 @@
+package datlowashere.project.rcoffee.ui.view.activity.account
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import datlowashere.project.rcoffee.R
+
+class ResetPasswordRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reset_password_request)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
